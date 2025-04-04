303d5e2493b242d72ce211e2956193d57e6f4a4a
Sardin20 <107612667+Sardin20@users.noreply.github.com>
2025-04-04 11:27:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const multer = require('multer');
 const helmet = require('helmet');
 const compression = require('compression');
 const morgan = require('morgan');
-require('dotenv').config(); // Load environment variables
+require('.env').config(); // Load environment variables
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
